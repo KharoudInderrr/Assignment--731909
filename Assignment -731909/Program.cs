045a035492e77cac316fafb7a87a59a6e00cd56a
visualstudio <visualstudio@122-PC19>
2019-03-06 11:25:14 -0500

    lab5

--- Assignment -731909/Program.cs
+++ Assignment -731909/Program.cs
@@ -9,48 +9,48 @@ using System.Threading.Tasks;
 
 //Student name - jagroop Singh   Student id - C0730507
 
-    // Assignment 2
-    // March 6, 2019
+// Assignment 2
+// March 6, 2019
 
 
 namespace delegateAndEvents
 {
-       
-            
 
 
-        
 
-        public class DelegateExcercises
+
+
+    
+
+
+    public class Program
+    {
+        public static void Main()
         {
-        public delegate int MyDelegate(int intvalue);
-            public int Method1(int intMethod1)
-            {
-            return intMethod1 * 2;
-            }
-            public int Method2(int intMethod2)
-            {
-                return int intMethod2 * 10;
-            }
-        public voidMethod3()
-        { 
-        MyDelegate myDelegate = new MyDelegate(Method1);
-        int result1 = myDelegate(10);
-        System.Console.WriteLine(result1);
-      myDelegate = new MyDelegate(Method2);
-        int result2 = myDelegate(10);
-        System.Console.WriteLine(result2);
-    }
-        static void Main(string[] args)
-        {
-            DelegateExcercises a = new DelegateExcercises();
-            a.Method3();
-
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
         }
     }
-    
-        
-    
+
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate();
+
+        void Method1()
+        {
+            System.Console.WriteLine("MyDelegate");
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+    }
+}
+
+
+
 
 
 
