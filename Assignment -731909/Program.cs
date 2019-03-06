3af7b2abe74cff52aa305d5330c3f42ae86dda48
visualstudio <visualstudio@122-PC19>
2019-03-06 11:10:18 -0500

    lab 2 commit - C0731909/C0730507

--- Assignment -731909/Program.cs
+++ Assignment -731909/Program.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//Student name - Inderjeet Singh   Student id - C0731909
+
+//Student name - jagroop Singh   Student id - C0730507
+
+    // Assignment 2
+    // March 6, 2019
+
+
 namespace delegateAndEvents
 {
        
-            //Name 1 - Inderjeet Singh
-            //ID - c0731909
-
-            //NAME 2 - Jagroop Singh
-            //Id - C0730507
+            
 
 
         
@@ -27,7 +32,7 @@ namespace delegateAndEvents
             public void Method2()
             {
                 MyDelegate myDelegate = new MyDelegate(Method1);
-                myDelegate();
+                myDelegate(50);
             }
         }
     }
